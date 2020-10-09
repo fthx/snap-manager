a902a6665c6648cf7038c4f8ce16b68ed12f474e
fthx <grouik.grouik@yahoo.fr>
2020-10-09 23:55:28 +0200

    Add extra actions submenu and code cleanup

--- extension.js
+++ extension.js
@@ -14,19 +14,24 @@ const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
 // here you can add/remove/hack the actions
-var menuActions = 	[	
-						["Refresh installed snaps", "echo Refresh installed snaps; echo; snap refresh"],
+var menuActions =	[	
+						["List snap updates", "echo List snap updates; echo; snap refresh --list"],
 						["List installed snaps", "echo List installed snaps; echo; snap list"],
 						["Recent snap changes", "echo Recent snap changes; echo; snap changes"],
-						["Install snap...", "echo Install snap...; echo; echo -e Enter snap name:; read snapname; echo Enter channel:; read snapchannel; echo; snap install $snapname --channel=$snapchannel"],
-						["Remove snap...", "echo Remove snap...; echo; echo -e Enter snap name:; read snapname; echo; snap remove $snapname"],
-						["Change snap channel...", "echo Change snap channel...; echo; echo -e Enter snap name:; read snapname; echo Enter channel:; read snapchannel; echo; snap refresh $snapname --channel=$snapchannel"],
-						["Snap info...", "echo Snap info...; echo; echo -e Enter snap name:; read snapname; echo; snap info $snapname"],
-						["Find snap...", "echo Find snap...; echo; echo -e Enter one search word:; read snapsearch; echo; snap find $snapsearch"],
-						["Open Snap Store website", "xdg-open https://snapcraft.io/store"],
-						
+						["Refresh installed snaps", "echo Refresh installed snaps; echo; snap refresh"],
+						["Install snap...", "echo Install snap...; echo; read -p 'Enter snap name: ' snapname; read -p 'Enter channel (void=default): ' snapchannel; echo; snap install $snapname --channel=$snapchannel"],
+						["Remove snap...", "echo Remove snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap remove $snapname"]
 					];
 
+// here you can add/remove/hack the extra actions					
+var menuExtraActions = 	[
+							["Change snap channel...", "echo Change snap channel...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; read -p 'Enter channel: ' snapchannel; echo; snap refresh $snapname --channel=$snapchannel"],
+							["Snap info...", "echo Snap info...; echo; read -p 'Enter snap name: ' snapname; echo; snap info $snapname"],
+							["Find snap...", "echo Find snap...; echo; read -p 'Enter one word to search: ' snapsearch; echo; snap find $snapsearch"],
+							["Hold auto refresh for one hour", "snap set system refresh.hold=$(date --iso-8601=seconds -d '1 hour')"],
+							["Hold auto refresh for one day", "snap set system refresh.hold=$(date --iso-8601=seconds -d '1 day')"],
+							["Hold auto refresh for one week", "snap set system refresh.hold=$(date --iso-8601=seconds -d '1 week')"]
+						];
 
 let SnapMenu = GObject.registerClass(
 class SnapMenu extends PanelMenu.Button {
@@ -50,20 +55,43 @@ class SnapMenu extends PanelMenu.Button {
         hbox.add_child(label);
         hbox.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
         this.add_child(hbox);
-
+		
+		// main actions
 		menuActions.forEach(this._addSnapMenuItem.bind(this));
+		
+		// extra actions
+		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+		this.submenu = new PopupMenu.PopupSubMenuMenuItem('Extra actions');
+		this.menu.addMenuItem(this.submenu);
+		menuExtraActions.forEach(this._addSnapSubmenuItem.bind(this));
+		
+		// open Snap Store in default browser
+		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+		this.menu.addAction("Open Snap Store website", event => {
+			Util.trySpawnCommandLine("xdg-open https://snapcraft.io/store");
+		})
     }
     
     _addSnapMenuItem(item, index, array) {
-    	if (index == 3 || index == 6 || index == 8) {
-    		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-    	}
+    	if (index == 3) {
+	    		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+	    }
 	    this.menu.addAction(item[0],event => {
 	    	try {
-    			Util.trySpawnCommandLine("gnome-terminal -x bash -c '" + item[1] + "; echo; echo -----------------------; echo Press enter to close...; read line'");
-				} catch(err) {
+    			Util.trySpawnCommandLine("gnome-terminal -x bash -c \"" + item[1] + "; echo; echo ---------------------------; read -n 1 -s -r -p 'Press any key to close...'\"");
+			} catch(err) {
     			Main.notify("Error: unable to execute command in GNOME Terminal");
-				}
+			}
+	    });
+	}
+	
+	_addSnapSubmenuItem(item, index, array) {
+	    this.submenu.menu.addAction(item[0],event => {
+	    	try {
+    			Util.trySpawnCommandLine("gnome-terminal -x bash -c \"" + item[1] + "; echo; echo ---------------------------; read -n 1 -s -r -p 'Press any key to close...'\"");
+			} catch(err) {
+    			Main.notify("Error: unable to execute command in GNOME Terminal");
+			}
 	    });
 	}
 });
