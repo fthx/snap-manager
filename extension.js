be6b93000de62f82db2074809944d21a4a081b84
fthx <grouik.grouik@yahoo.fr>
2020-10-11 12:06:48 +0200

    Add a 'Refresh options' submenu

--- extension.js
+++ extension.js
@@ -15,9 +15,9 @@ const PopupMenu = imports.ui.popupMenu;
 
 // here you can add/remove/hack the actions
 var menuActions =	[	
-						["List snap updates", "echo List snap updates; echo; snap refresh --list"],
 						["List installed snaps", "echo List installed snaps; echo; snap list"],
 						["Recent snap changes", "echo Recent snap changes; echo; snap changes"],
+						["List snap updates", "echo List snap updates; echo; snap refresh --list"],
 						["Refresh installed snaps", "echo Refresh installed snaps; echo; snap refresh"],
 						["Install snap...", "echo Install snap...; echo; read -p 'Enter snap name: ' snapname; read -p 'Enter channel (void=default): ' snapchannel; echo; snap install $snapname --channel=$snapchannel"],
 						["Remove snap...", "echo Remove snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap remove $snapname"]
@@ -27,11 +27,17 @@ var menuActions =	[
 var menuExtraActions = 	[
 							["Change snap channel...", "echo Change snap channel...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; read -p 'Enter channel: ' snapchannel; echo; snap refresh $snapname --channel=$snapchannel"],
 							["Snap info...", "echo Snap info...; echo; read -p 'Enter snap name: ' snapname; echo; snap info $snapname"],
-							["Find snap...", "echo Find snap...; echo; read -p 'Enter one word to search: ' snapsearch; echo; snap find $snapsearch"],
-							["Hold auto refresh for one hour", "echo Hold auto refresh for one hour; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 hour')"],
-							["Hold auto refresh for one day", "echo Hold auto refresh for one day; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 day')"],
-							["Hold auto refresh for one week", "echo Hold auto refresh for one week; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 week')"]
+							["Find snap...", "echo Find snap...; echo; read -p 'Enter one word to search: ' snapsearch; echo; snap find $snapsearch"]
 						];
+						
+// here you can add/remove/hack the hold refresh time options
+var menuRefreshOptions =	[
+							["Refresh schedule", "echo Refresh schedule; echo; snap refresh --time"],
+							["Hold auto refresh for one hour", "echo Hold auto refresh for one hour; echo; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 hour'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
+							["Hold auto refresh for one day", "echo Hold auto refresh for one day; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 day'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
+							["Hold auto refresh for one week", "echo Hold auto refresh for one week; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 week'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
+							["Cancel refresh delay", "echo Cancel hold auto refresh delay; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '0 second'); echo; echo Refresh schedule; echo; snap refresh --time"]
+						]
 
 let SnapMenu = GObject.registerClass(
 class SnapMenu extends PanelMenu.Button {
@@ -40,7 +46,7 @@ class SnapMenu extends PanelMenu.Button {
 
         let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
         let icon = new St.Icon({
-        	// here you can change the menu icon
+        	// here you can change the menu icon (waiting for snap symbolic icon...)
             icon_name: 'applications-system-symbolic',
             style_class: 'system-status-icon',
         });
@@ -56,14 +62,18 @@ class SnapMenu extends PanelMenu.Button {
         hbox.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
         this.add_child(hbox);
 		
-		// main actions
+		// main menu
 		menuActions.forEach(this._addSnapMenuItem.bind(this));
 		
-		// extra actions
+		// extra actions submenu
 		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		this.submenu = new PopupMenu.PopupSubMenuMenuItem('Extra actions');
-		this.menu.addMenuItem(this.submenu);
-		menuExtraActions.forEach(this._addSnapSubmenuItem.bind(this));
+		this.submenu1 = new PopupMenu.PopupSubMenuMenuItem('Extra actions');
+		this.menu.addMenuItem(this.submenu1);
+		menuExtraActions.forEach(this._addExtraSubmenuItem.bind(this));
+		// refresh options submenu
+		this.submenu2 = new PopupMenu.PopupSubMenuMenuItem('Refresh options');
+		this.menu.addMenuItem(this.submenu2);
+		menuRefreshOptions.forEach(this._addRefreshSubmenuItem.bind(this));
 		
 		// open Snap Store in default browser
 		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
@@ -72,23 +82,36 @@ class SnapMenu extends PanelMenu.Button {
 		})
     }
     
+    // main menu items
     _addSnapMenuItem(item, index, array) {
     	if (index == 3) {
 	    		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 	    }
 	    this.menu.addAction(item[0],event => {
 	    	try {
-    			Util.trySpawnCommandLine("gnome-terminal -x bash -c \"" + item[1] + "; echo; echo ---------------------------; read -n 1 -s -r -p 'Press any key to close...'\"");
+    			Util.trySpawnCommandLine("gnome-terminal -x bash -c \"" + item[1] + "; echo; echo --; read -n 1 -s -r -p 'Press any key to close...'\"");
 			} catch(err) {
     			Main.notify("Error: unable to execute command in GNOME Terminal");
 			}
 	    });
 	}
 	
-	_addSnapSubmenuItem(item, index, array) {
-	    this.submenu.menu.addAction(item[0],event => {
+	// extra actions submenu items
+	_addExtraSubmenuItem(item, index, array) {
+	    this.submenu1.menu.addAction(item[0],event => {
 	    	try {
-    			Util.trySpawnCommandLine("gnome-terminal -x bash -c \"" + item[1] + "; echo; echo ---------------------------; read -n 1 -s -r -p 'Press any key to close...'\"");
+    			Util.trySpawnCommandLine("gnome-terminal -x bash -c \"" + item[1] + "; echo; echo --; read -n 1 -s -r -p 'Press any key to close...'\"");
+			} catch(err) {
+    			Main.notify("Error: unable to execute command in GNOME Terminal");
+			}
+	    });
+	}
+	
+	// refresh options submenu items
+	_addRefreshSubmenuItem(item, index, array) {
+	    this.submenu2.menu.addAction(item[0],event => {
+	    	try {
+    			Util.trySpawnCommandLine("gnome-terminal -x bash -c \"" + item[1] + "; echo; echo --; read -n 1 -s -r -p 'Press any key to close...'\"");
 			} catch(err) {
     			Main.notify("Error: unable to execute command in GNOME Terminal");
 			}
