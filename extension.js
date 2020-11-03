b5edd3c6a56f08e0d871c5615431888361a7ee2d
fthx <23138504+fthx@users.noreply.github.com>
2020-11-03 23:36:09 +0100

    Add more menu actions (revert, Connections submenu)

--- extension.js
+++ extension.js
@@ -32,29 +32,37 @@ const WAIT_REFRESH_LIST = 10;
 var menuActions =	[	
 						["List installed snaps", "echo List installed snaps; echo; snap list"],
 						["List recent snap changes", "echo List recent snap changes; echo; snap changes"],
-						["List available snap updates", "echo List available snap updates; echo; snap refresh --list"],
+						["List available snap refresh", "echo List available snap refresh; echo; snap refresh --list"],
 						["Refresh installed snaps", "echo Refresh installed snaps; echo; snap refresh"],
 						["Install snap...", "echo Install snap...; echo; read -p 'Enter snap name: ' snapname; echo; echo Available channels:; snap info $snapname | awk '/channels:/{y=1;next}y'; echo; read -p 'Enter channel (void=default): ' snapchannel; echo; snap install $snapname --channel=$snapchannel"],
 						["Remove snap...", "echo Remove snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap remove $snapname"]
 					];
 
-// here you can add/remove/hack the extra actions					
-var menuExtraActions = 	[
-							["Refresh snap channel...", "echo Refresh snap channel...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; echo Available channels:; snap info $snapname | awk '/channels:/{y=1;next}y'; echo; read -p 'Enter new channel: ' snapchannel; echo; snap refresh $snapname --channel=$snapchannel"],
-							["Snap info...", "echo Snap info...; echo; read -p 'Enter snap name: ' snapname; echo; snap info --verbose $snapname; echo; snap connections $snapname"],
-							["Find snap...", "echo Find snap...; echo; read -p 'Enter one word to search: ' snapsearch; echo; snap find $snapsearch"],
-							["Enable snap...", "echo Enable snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap enable $snapname"],
-							["Disable snap...", "echo Disable snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap disable $snapname"]
-						];
+// here you can add/remove/hack the snap options					
+var menuSnapOptions = [
+	["Snap info...", "echo Snap info...; echo; read -p 'Enter snap name: ' snapname; echo; snap info --verbose $snapname"],
+	["Refresh snap channel...", "echo Refresh snap channel...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; echo Available channels:; snap info $snapname | awk '/channels:/{y=1;next}y'; echo; read -p 'Enter new channel: ' snapchannel; echo; snap refresh $snapname --channel=$snapchannel"],
+	["Revert snap refresh...", "echo Revert snap refresh...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap revert $snapname"],
+	["Enable snap...", "echo Enable snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap enable $snapname"],
+	["Disable snap...", "echo Disable snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; snap disable $snapname"]							
+];
+						
+// here you can add/remove/hack the snap connections					
+var menuSnapConnections = [
+	["List available interfaces", "echo List available interfaces; echo; snap interface"],
+	["List snap connections...", "echo List snap connections...; echo; read -p 'Enter snap name: ' snapname; echo; echo Available connections:; snap connections $snapname"],
+	["Connect snap...", "echo Connect snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; echo Available connections:; snap connections $snapname; echo; read -p 'Enter interface to connect: ' snapconnection; echo; snap connect $snapname:$snapconnection"],
+	["Disconnect snap...", "echo Disconnect snap...; echo; snap list; echo; read -p 'Enter snap name: ' snapname; echo; echo Available connections:; snap connections $snapname; echo; read -p 'Enter interface to disconnect: ' snapconnection; echo; snap disconnect $snapname:$snapconnection"],
+];
 						
 // here you can add/remove/hack the hold refresh time options
-var menuRefreshOptions =	[
-							["Refresh schedule", "echo Refresh schedule; echo; snap refresh --time"],
-							["Hold auto refresh for one hour", "echo Hold auto refresh for one hour; echo; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 hour'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
-							["Hold auto refresh for one day", "echo Hold auto refresh for one day; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 day'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
-							["Hold auto refresh for one week", "echo Hold auto refresh for one week; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 week'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
-							["Cancel auto refresh delay", "echo Cancel auto refresh delay; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '0 second'); echo; echo Refresh schedule; echo; snap refresh --time"]
-						];
+var menuRefreshOptions = [
+	["Refresh schedule", "echo Refresh schedule; echo; snap refresh --time"],
+	["Hold auto refresh for one hour", "echo Hold auto refresh for one hour; echo; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 hour'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
+	["Hold auto refresh for one day", "echo Hold auto refresh for one day; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 day'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
+	["Hold auto refresh for one week", "echo Hold auto refresh for one week; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '1 week'); echo; echo Refresh schedule; echo; snap refresh --time | grep hold"],
+	["Cancel auto refresh delay", "echo Cancel auto refresh delay; echo; sudo snap set system refresh.hold=$(date --iso-8601=seconds -d '0 second'); echo; echo Refresh schedule; echo; snap refresh --time"]
+];
 
 // define local Gio snap symbolic icon					
 var iconPath = Me.path + "/snap-symbolic.svg";
@@ -81,15 +89,20 @@ class SnapMenu extends PanelMenu.Button {
 		// main menu
 		menuActions.forEach(this._addSnapMenuItem.bind(this));
 		
-		// extra actions submenu
+		// snap configuration submenu
 		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		this.submenu1 = new PopupMenu.PopupSubMenuMenuItem('Extra actions');
+		this.submenu1 = new PopupMenu.PopupSubMenuMenuItem('Snap options');
 		this.menu.addMenuItem(this.submenu1);
-		menuExtraActions.forEach(this._addExtraSubmenuItem.bind(this));
+		menuSnapOptions.forEach(this._addOptionsSubmenuItem.bind(this));
+		
+		// snap connections submenu
+		this.submenu2 = new PopupMenu.PopupSubMenuMenuItem('Snap connections');
+		this.menu.addMenuItem(this.submenu2);
+		menuSnapConnections.forEach(this._addConnectSubmenuItem.bind(this));
 		
 		// refresh options submenu
-		this.submenu2 = new PopupMenu.PopupSubMenuMenuItem('Refresh options');
-		this.menu.addMenuItem(this.submenu2);
+		this.submenu3 = new PopupMenu.PopupSubMenuMenuItem('Refresh options');
+		this.menu.addMenuItem(this.submenu3);
 		menuRefreshOptions.forEach(this._addRefreshSubmenuItem.bind(this));
 		
 		// open Snap Store in default browser
@@ -185,16 +198,23 @@ class SnapMenu extends PanelMenu.Button {
 	    });
 	};
 	
-	// extra actions submenu items
-	_addExtraSubmenuItem(item, index, array) {
+	// snap options submenu items
+	_addOptionsSubmenuItem(item, index, array) {
 	    this.submenu1.menu.addAction(item[0],event => {
 	    	this._executeAction(item[1])
 	    });
-	}
+	};
+	
+	// snap connections submenu items
+	_addConnectSubmenuItem(item, index, array) {
+	    this.submenu2.menu.addAction(item[0],event => {
+	    	this._executeAction(item[1])
+	    });
+	};
 	
 	// refresh options submenu items
 	_addRefreshSubmenuItem(item, index, array) {
-	    this.submenu2.menu.addAction(item[0],event => {
+	    this.submenu3.menu.addAction(item[0],event => {
 	    	this._executeAction(item[1])
 	    });
 	}
